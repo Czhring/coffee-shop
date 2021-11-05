a0e5d91a89c06a3cfd5cfa04d50504b3254d50fa
Ring <ring@ringdembp.attlocal.net>
2021-11-05 11:17:57 -0700

    updated work

---
+++ src/component/Cart/Cart.js
@@ -0,0 +1,31 @@
+import Button from 'react-bootstrap/Button'
+import './Cart.css'
+
+function Cart (props){
+    console.log(props)
+
+
+
+    let myDrink = ''
+    if(props.drink){
+        myDrink = props.drink.map((coffee , index) => {
+        return(
+            <div>
+            <h3>{coffee.title}</h3>
+            <button onClick={()=>props.removeDrinkFromList(index)}> Remove</button>
+        </div>)
+        })
+    }
+
+    return(
+    
+    <div>
+       <h1> My Cart</h1>
+       <p>{myDrink}</p>
+        <Button variant="dark">Pay</Button>{' '}
+    </div>
+    
+)
+}
+
+export default Cart
